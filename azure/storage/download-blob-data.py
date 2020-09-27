7491dfcf78c209d741ac55315b07f9a657e788a0
MLStruckmann <magnus.struckmann@gmail.com>
2020-09-27 16:44:56 +0200

    Implement Azure download and upload

--- azure/storage/download-blob-data.py
+++ azure/storage/download-blob-data.py
@@ -1,25 +1,57 @@
 import configparser
 import os
 from pathlib import Path
+from azure.storage.blob import BlobServiceClient
 
-config = configparser.ConfigParser()
+# Define variables
+container_name = "upload-test-3"
+blob_name = None # set blob_name (str) if you only want to download a single file from specified container
+download_folder = "/home/magnus/Downloads"
 
 # Read config
-file_path = Path(os.path.abspath(__file__))
-folder_directory = file_path.parent.parent
+config = configparser.ConfigParser()
+current_file_path = Path(os.path.abspath(__file__))
+folder_directory = current_file_path.parent.parent
 config_path = os.path.join(folder_directory,"azure-config.ini")
 config.read(config_path)
 
-# Config read example
-print(config.sections())
-print(config["azure_blob_storage"]["connection_string"])
+# Assign variables from config
+blob_account_url = config["azure_blob_storage"]["connection_string"]
 
-'''import os, uuid
-from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, __version__
+# List all containers in storage account
+blob_service_client = BlobServiceClient.from_connection_string(conn_str=blob_account_url)
+all_containers = blob_service_client.list_containers()
+print("\n\nContainers in storage account:")
+for container in all_containers:
+    print("\t" + container["name"])
 
-try:
-    print("Azure Blob storage v" + __version__ + " - Python quickstart sample")
-    # Quick start code goes here
-except Exception as ex:
-    print('Exception:')
-    print(ex)'''
+# List files in container
+container_client = blob_service_client.get_container_client(container_name)
+blob_list = container_client.list_blobs()
+print("\nFiles in container",container_name+":")
+for blob in blob_list:
+    print("\t" + blob.name)
+
+# Download files
+def download_blob(blob_name_download):
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name_download)
+    download_file_path = os.path.join(download_folder,blob_name_download)
+    print("\t"+blob_name_download)
+    with open(download_file_path, "wb") as download_file:
+        download_file.write(blob_client.download_blob().readall())
+
+print("\nStart download from",container_name)
+print("Downloading to",download_folder+":")
+
+if blob_name == None:
+    blob_list = container_client.list_blobs()
+    for blob in blob_list:
+        download_blob(blob_name_download=blob.name)
+
+elif type(blob_name) == str:
+    download_blob(blob_name_download=blob_name)
+
+else:
+    raise TypeError("blob_name was not type(str) or 'None'")
+
+print("\n")
