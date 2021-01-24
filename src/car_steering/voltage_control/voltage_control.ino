1fb037ddbfbb5a99a35a0bbffc03c8d8b819ca3a
dev <dev@dev.com>
2021-01-24 17:00:21 +0100

    Include latest Arduino sketch

--- src/car_steering/voltage_control/voltage_control.ino
+++ src/car_steering/voltage_control/voltage_control.ino
@@ -1,64 +1,40 @@
-const int trackPin = 3;      // the pin that the track is connected to
-const int ledPin = 13;       // the pin that the LED is attached to
+int sensorPinR = A0;
+int sensorPinL = A1;
+
+int sensorValueL = 0;
+int sensorValueR = 0;
+int output_pin = 5;
+
 int incomingByte;            // a variable to read incoming serial data into
 
-
 void setup() {
-
-  Serial.begin(9600);        // initialize serial communication
-  pinMode(ledPin, OUTPUT);   // initialize the LED pin as an output
-  pinMode(trackPin, OUTPUT); // initialize the track pin as an output
-  
+  Serial.begin(9600);
+//  pinMode(output_pin, OUTPUT);
 }
 
 void loop() {
-  // see if there's incoming serial data:
+  sensorValueL = analogRead(sensorPinL);
+  sensorValueR = analogRead(sensorPinR);
+
+  Serial.print("Left:");
+  Serial.print(sensorValueL);
+  Serial.print("-");
+  Serial.print("Right:");
+  Serial.println(sensorValueR);
+
+//  analogWrite(output_pin, 255);
+
   if (Serial.available() > 0) {
     // read the oldest byte in the serial buffer:
     incomingByte = Serial.read();
-    // if it's a number, (ASCII 72), set voltage:
     if (incomingByte == 'q') {
-      analogWrite(trackPin, 0);
+      analogWrite(output_pin, 160);
     }
     if (incomingByte == 'w') {
-      analogWrite(trackPin, 50);
+      analogWrite(output_pin, 140);
     }
     if (incomingByte == 'e') {
-      analogWrite(trackPin, 75);
+      analogWrite(output_pin, 120);
     }
-    if (incomingByte == 'r') {
-      analogWrite(trackPin, 100);
-    }
-    if (incomingByte == 't') {
-      analogWrite(trackPin, 125);
-    }
-    if (incomingByte == 'z') {
-      analogWrite(trackPin, 150);
-    }
-    if (incomingByte == 'u') {
-      analogWrite(trackPin, 175);
-    }
-    if (incomingByte == 'i') {
-      analogWrite(trackPin, 200);
-    }
-    if (incomingByte == 'o') {
-      analogWrite(trackPin, 225);
-    }
-    if (incomingByte == 'p') {
-      analogWrite(trackPin, 255);
-    }
-
-    // if it's a capital T (ASCII 72), set resistance to 900 (step 90):
-    if (incomingByte == 'T') {
-      analogWrite(trackPin, 0);
-    }
-    // if it's a capital X (ASCII 72), turn on the LED:
-    if (incomingByte == 'X') {
-      digitalWrite(ledPin, HIGH);
-    }
-    // if it's an Y (ASCII 76) turn off the LED:
-    if (incomingByte == 'Y') {
-      digitalWrite(ledPin, LOW);
-    }
-  }
+}
 }
